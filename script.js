841e5d3db9a328a6c4b924b20ab4dbc16d21c442
Junior4144 <94208651+Junior4144@users.noreply.github.com>
2024-12-16 17:15:35 -0600

    more logic added, develop further

--- script.js
+++ script.js
@@ -19,19 +19,26 @@ function divide(num1, num2){
 let firstNumber = null;
 let operator = '';
 let secondNumber = null;
-let firstSelection = true;
-
-let operatorSelection = false;
 
 
-function operate(operator, firstNumber, secondNumber){
+
+
+function operate(){
 
     if (operator == "+"){
         //add(firstNumber,secondNumber);
-        display.textContent = add(firstNumber,secondNumber).toString();
+        let x = add(firstNumber,secondNumber).toString();
+        console.log(`${firstNumber} ${operator} ${secondNumber} equals ${x}`)
+        display.textContent = x;
+        firstNumber = x;
+        
     }
     if (operator == "-"){
-        display.textContent = subtract(firstNumber,secondNumber).toString();
+        let x = subtract(firstNumber,secondNumber).toString();
+        display.textContent = x;
+        firstNumber = x;
+        console.log(`${firstNumber} ${operator} ${secondNumber} equals ${x}`)
+        
     }
     if (operator == "*"){
         multiply(firstNumber,secondNumber);
@@ -48,64 +55,120 @@ const sevenButton = document.querySelector('.seven-btn');
 const display = document.querySelector('.display');
 const addButton = document.querySelector('.add-btn');
 const subtractButton = document.querySelector('.subtract-btn');
+const preview = document.querySelector('.preview');
+
+let firstNumberSelection = true;
+let operatorSelection = false;
+let secondNumberSelection = false;
+let displaySelection = false;
+
+let firstSelection = true;
+let secondSelection = true;
+
 
 
 equalsButton.addEventListener('click', () =>{
-    secondNumber = display.textContent;
-    operate(operator, firstNumber, secondNumber);
+
+    if(!operatorSelection){
+        secondNumber = display.textContent;
+    }
+    
+    preview.textContent = firstNumber + " + "  + secondNumber + " =";
+    secondNumberSelection = false;
+    displaySelection = true;
+    operatorSelection = false;
+    operate();
+    console.log(`--------------------------------`)
+    console.log(`first: ${firstNumberSelection}`)
+    console.log(`operator: ${operatorSelection}`)
+    console.log(`second: ${secondNumberSelection}`)
+    console.log(`display: ${displaySelection}`)
+    console.log(`--------------------------------`)
 });
 sevenButton.addEventListener('click', () =>{
-    operatorSelection = false;
+
+    if(operatorSelection){
+        display.textContent = '';
+    }
+    
+    if(firstNumberSelection == false && !secondNumberSelection){
+        secondNumberSelection = true;
+        display.textContent = '';
+    }
+
     if(display.textContent == "0") display.textContent = '';
 
     if (!firstSelection){
         display.textContent = '';
         firstSelection = true;
     }
+    if(!secondSelection){
+        display.textContent = '';
+        secondSelection = true;
+    }
+
+    operatorSelection = false;
     display.textContent += 7;
+    console.log(`--------------------------------`)
+    console.log(`first: ${firstNumberSelection}`)
+    console.log(`operator: ${operatorSelection}`)
+    console.log(`second: ${secondNumberSelection}`)
+    console.log(`--------------------------------`)
 
 });
 addButton.addEventListener('click', () =>{
     
-    if (operatorSelection) {
-        operator = "+";
-        console.log(operator)
-        return;
-    };
-    if(operator != ''){
-        const clickEvent = new Event('click');
-        equalsButton.dispatchEvent(clickEvent);
+    //firstNum = null
+    if(firstNumberSelection){ // if firstNumberSelection is true -> 
+        firstNumber = display.textContent;
+        firstNumberSelection = false;
+        firstSelection = false;
+        preview.textContent = firstNumber + " +" ;
     }
 
-    firstNumber = display.textContent;
-    firstSelection = false;
+    if(secondNumberSelection){ // if secondNumberSelection is true -> 77 + 77 +
+        secondNumber = display.textContent;
+        console.log("hello");
+        operate();
+        
+        preview.textContent = firstNumber + " +";
+        secondNumberSelection = false;
+    }
+    if(display){
+        preview.textContent = firstNumber + " +";
+    }
+
+
     operator = "+";
     operatorSelection = true;
     console.log(operator)
 
+    console.log(`--------------------------------`)
+    console.log(`first: ${firstNumberSelection}`)
+    console.log(`operator: ${operatorSelection}`)
+    console.log(`second: ${secondNumberSelection}`)
+    console.log(`--------------------------------`)
+
 });
 
 subtractButton.addEventListener('click', () =>{
-    if(operatorSelection) {
-        operator = "-";
-        console.log(operator)
-        return
-    };
-
+    if(firstNumberSelection){ // if firstNumberSelection is true -> 
+        firstNumber = display.textContent;
+        firstNumberSelection = false;
+        firstSelection = false;
+    }
+    if(secondNumberSelection){ // if secondNumberSelection is true -> 
+        secondNumber = display.textContent;
+    }
     
-    if(operator != ''){
+    if(operator != '' && !operatorSelection ){
         console.log(operator);
         const clickEvent = new Event('click');
         equalsButton.dispatchEvent(clickEvent);
     }
-    firstNumber = display.textContent;
-    firstSelection = false;
     operator = "-";
     operatorSelection = true;
     console.log(operator)
-
-    //
-    
 
 });
 
